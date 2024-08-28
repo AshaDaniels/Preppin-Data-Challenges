9bdfeb25efb06509f4fe8c4c8de0686edfeb9f3e
AshaDaniels <165888998+AshaDaniels@users.noreply.github.com>
2024-08-28 15:14:19 +0100

    Update 2023_Wk_1.sql

--- 2023/2023_Wk_1.sql
+++ 2023/2023_Wk_1.sql
@@ -1,18 +1,22 @@
--- Query 1: Total Values by Bank
--- This query calculates the total transaction values grouped by bank.
--- It uses the SPLIT_PART function to extract the bank name from the TRANSACTION_CODE.
+-- Techniques:
+-- 1. Splitting strings with SPLIT_PART
+-- 2. Case Statements
+-- 3. Converting strings to dates
 
+-- Split the Transaction Code to extract the letters at the start of the transaction code. These identify the bank who processes the transaction (help)
+-- Rename the new field with the Bank code 'Bank'. 
+-- Rename the values in the Online or In-person field, Online of the 1 values and In-Person for the 2 values. 
+-- Change the date to be the day of the week (help)
+-- Different levels of detail are required in the outputs. You will need to sum up the values of the transactions in three ways (help):
+
+-- 1. Total Values of Transactions by each bank
 SELECT SUM(value),
     SPLIT_PART(TRANSACTION_CODE, '-', 1) AS "Bank"
 FROM pd2023_wk01
 GROUP BY "Bank"
 LIMIT 5;
 
--- Query 2: Total Values by Bank, Day of the Week, and Transaction Type
--- This query calculates the total transaction values grouped by bank, day of the week, 
--- and transaction type (Online or In-Person). It uses the CASE statement to classify transactions
--- and the DAYNAME function to extract the day of the week from the transaction date.
-
+-- 2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
 SELECT SUM(value),
     SPLIT_PART(TRANSACTION_CODE, '-', 1) AS "Bank",
     CASE 
@@ -24,10 +28,7 @@ FROM pd2023_wk01
 GROUP BY 2, 3, 4
 LIMIT 5;
 
--- Query 3: Total Values by Bank and Customer Code
--- This query calculates the total transaction values grouped by bank and customer code.
--- It extracts the bank name from the TRANSACTION_CODE using SPLIT_PART.
-
+-- 3. Total Values by Bank and Customer Code
 SELECT SUM(value), customer_code,
     SPLIT_PART(TRANSACTION_CODE, '-', 1) AS "Bank"
 FROM pd2023_wk01
