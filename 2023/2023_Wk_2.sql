7893aa8be61588f1ca20e92138048baf79cc7366
AshaDaniels <165888998+AshaDaniels@users.noreply.github.com>
2024-08-28 15:12:34 +0100

    Update 2023_Wk_2.sql

--- 2023/2023_Wk_2.sql
+++ 2023/2023_Wk_2.sql
@@ -1,8 +1,15 @@
--- This code creates bank account numbers via the below
--- INNER JOIN
--- String Concatenation
--- String Manipulation
--- Data Type Conversion
+-- Techniques:
+-- 1. Removing substrings with REPLACE()
+-- 2. Joining tables
+-- 3. Concatenations
+
+-- In the Transactions table, there is a Sort Code field which contains dashes. We need to remove these so just have a 6 digit string
+-- Use the SWIFT Bank Code lookup table to bring in additional information about the SWIFT code and Check Digits of the receiving bank account
+-- Add a field for the Country Code
+-- Hint: all these transactions take place in the UK so the Country Code should be GB
+-- Create the IBAN as above
+-- Hint: watch out for trying to combine sting fields with numeric fields - check data types
+-- Remove unnecessary fields
 
 SELECT 
     'GB' || CHECK_DIGITS || SWIFT_CODE || REPLACE(SORT_CODE, '-', '') || TO_VARCHAR(ACCOUNT_NUMBER) AS CONCATENATED_SORT_CODE
